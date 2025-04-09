0221ef179add5430c462440451080b68bb5633bc
Quynh Tran <87471005+quinmeh@users.noreply.github.com>
2025-04-09 01:18:44 -0700

    intialize variables and create recursive function that take in data as parameter

---
+++ src/r/recursion.R
@@ -0,0 +1,23 @@
+#data <- read.RDS()
+#flag = FALSE
+#i = 1
+#n = numrows(data)
+#
+#Do until(flag==TRUE}{
+#  
+#  (if added url to the dataframe){
+#    n = n+1
+#  }
+#  i = 1 +1
+#  if ( i ==n){flag==TRUE}
+#}
+
+#read RDS data
+data <- readRDS()
+flag <- FALSE
+i <- 1
+n <- nrow(data)
+
+#recursive function that take in data as parameter
+recursive_add_url <- function(data) {
+}
