280251daaea948ebb47bcd061b71de21b7d63648
dudkk <dnkhuu@ucdavis.edu>
2025-04-10 16:07:36 -0700

    updated get_html so it can be generalized for all df and update existing raw_xml

--- src/r/get_html.R
+++ src/r/get_html.R
@@ -1,3 +1,6 @@
+# Write a function(url, ID = optional) that get raw xml for url then adds a 
+# new row if there is no existing ID, otherwise updates the value in the dataframe.
+
 get_html <- function(url, ID) {
   # check if the given url exists in the df and if so, print its raw_xml 
   url_exist <- url == lib_guides_2025_02_28[, "url"]
@@ -14,4 +17,19 @@ get_html <- function(url, ID) {
    }
 }
 
-# Next task: update value in the df
+# generalize to any df + update raw_xml value
+get_html <- function(url, dataframes, ID) {
+  # check if the given url exists in the df and if so, print its raw_xml 
+  url_exist <- url == dataframes[, "url"]
+  dataframes[url_exist, "raw_xml"] <- fetch_url_contents(url_exist) # look
+  
+  # if the given url does not exist in the df already, add a new row to the df
+  if (sum(url_exist) == 0) {
+    new_url <- data.frame(
+      ID = nrow(dataframes) + 1,
+      url = url,
+      raw_xml = fetch_url_contents(url)
+    )
+    dataframes <- rbind(dataframes, new_url)
+  }
+}
