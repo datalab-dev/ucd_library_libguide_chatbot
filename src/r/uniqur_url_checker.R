17f865a36b9449079b945ad25f988fbda243ad9c
dudkk <dnkhuu@ucdavis.edu>
2025-04-10 16:26:38 -0700

    remove duplicated files uniqur_url_checker.R

--- src/r/uniqur_url_checker.R
+++
@@ -1,8 +0,0 @@
-unique_url_checker <- function(url) {
-  url_exist <- url == lib_guides_2025_02_28[, "url"]
-  print(lib_guides_2025_02_28[url_exist, "ID"])
-  
-  if (sum(url_exist) == 0) {
-    print(NA)
-  }
-}
