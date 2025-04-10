8017bbefa08299a026b4eb8c7a85a294714a63b6
Your Name <yyshuang@ucdavis.edu>
2025-04-10 11:40:52 -0700

    created get html function that returns raw xml for url and adds new row; next need to make the function also update value in the df

---
+++ src/r/get_html.R
@@ -0,0 +1,17 @@
+get_html <- function(url, ID) {
+  # check if the given url exists in the df and if so, print its raw_xml 
+  url_exist <- url == lib_guides_2025_02_28[, "url"]
+   print(lib_guides_2025_02_28[url_exist, "raw_xml"])
+   
+   # if the given url does not exist in the df already, add a new row to the df
+   if (sum(url_exist) == 0) {
+     new_url <- data.frame(
+       ID = nrow(lib_guides_2025_02_28) + 1,
+       url = url,
+       raw_xml = fetch_url_contents(url)
+     )
+     lib_guides_2025_02_28 <- rbind(lib_guides_2025_02_28, new_url)
+   }
+}
+
+# Next: update value in the df
