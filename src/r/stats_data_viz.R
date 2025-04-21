642d90ddff0a6f1475ea576f14178ef18228cf38
Your Name <yyshuang@ucdavis.edu>
2025-04-21 14:15:45 -0700

    modified the functions to make it work with the data frame that contains subchunk text

--- src/r/stats_data_viz.R
+++ src/r/stats_data_viz.R
@@ -6,10 +6,10 @@ library(grid)
 # function that adds word counts and character counts to df
 get_basic_stats <- function(df, chunk){
   # word count
-  word_count <- str_count(df$chunk, boundary("word"))
+  word_count <- str_count(df[[chunk]], boundary("word"))
   
   # character count
-  chunk_cat <- str_remove_all(df$chunk, " ")
+  chunk_cat <- str_remove_all(df[[chunk]], " ")
   character_count <- str_length(chunk_cat)
   
   # add word_count and character_count as new columns of df
@@ -21,7 +21,7 @@ get_basic_stats <- function(df, chunk){
 # function that generates boxplots 
 make_box <- function(df, col, title) {
   ggplot(df) +
-    aes(x = col) +
+    aes(x = {{col}}) +
     geom_boxplot() +
     labs(title = title)
 }
@@ -30,7 +30,7 @@ make_box <- function(df, col, title) {
 # function that generates histograms 
 histogram <- function(df, col, title, xlab) {
   ggplot(df) +
-    aes(x = col) +
+    aes(x = {{col}}) +
     geom_histogram(binwidth = 25)+
     labs(title = title,
          x = xlab,
@@ -40,13 +40,13 @@ histogram <- function(df, col, title, xlab) {
 
 
 # create and save the new df containing word and character counts
-df_count <- get_basic_stats(df, chunk)
+chunks_count <- get_basic_stats(text_chunks_df, "sub_chunks")
 saveRDS(df_count, file = "/Users/sebrina/sts195/data/word_character_count.rds")
 
 
 # create and save the box plots
-word_box <- make_box(df_count, word_count, "Word Counts of Subsession Text")
-character_box <- make_box(df_count, character_count, "Character Counts of Subsession Text")
+word_box <- make_box(chunks_count, word_count, "Word Counts of Subchunk Text")
+character_box <- make_box(chunks_count, character_count, "Character Counts of Subchunk Text")
 ggsave(plot = word_box, "/Users/sebrina/sts195/word_count_boxplot.pdf")
 ggsave(plot = character_box, "/Users/sebrina/sts195/character_count_boxplot.pdf")
 
@@ -60,15 +60,12 @@ summary_table <- function(df, col, filename){
   grid.table(table_df)
   dev.off()
 } 
-summary_table(df_count, "character_count", "/Users/sebrina/sts195/character_count_summary.pdf")
-summary_table(df_count, "word_count", "/Users/sebrina/sts195/word_count_summary.pdf")
+summary_table(chunks_count, "character_count", "/Users/sebrina/sts195/character_count_summary.pdf")
+summary_table(chunks_count, "word_count", "/Users/sebrina/sts195/word_count_summary.pdf")
 
 
 # create and save the histogram plots
-word_hist <- histogram(df_count, word_count, "Word Counts Bin by 25", "Word Counts")
-character_hist <- histogram(df_count, character_count, "Character Counts Bin by 25", "Character Counts")
+word_hist <- histogram(chunks_count, word_count, "Word Counts Bin by 25", "Word Counts")
+character_hist <- histogram(chunks_count, character_count, "Character Counts Bin by 25", "Character Counts")
 ggsave(plot = word_hist, "/Users/sebrina/sts195/word_count_hist.pdf")
 ggsave(plot = character_hist, "/Users/sebrina/sts195/character_count_hist.pdf")
-
-
-## Haven't test the above functions with actual data frame ##
