9c1b3f30cbd60d696fb5c4d397cd032ae1b55e7b
Your Name <yyshuang@ucdavis.edu>
2025-04-28 15:36:06 -0700

    generalize function and try llama3.2 model

--- src/r/ollamar.R
+++ src/r/ollamar.R
@@ -14,9 +14,33 @@ repeat {
     break
   }
   
-  response <- generate("llama3.2", user_input, output="text")
+  response <- generate("llama3.1", user_input, output="text")
   cat("Bot:", response, "\n")
 }
 
 # next step: 1) initialize chat history?
 #            2) try out diff models
+
+
+# generalize function
+ollamar_chatbot <- function(model){
+  pull(model)
+  
+  # setting up "chatbot" interface
+  repeat {
+    user_input <- readline(prompt = "You: ")
+    if (tolower(user_input) == "exit") {
+      break
+    }
+    
+  response <- generate(model, user_input, output = "text")
+  cat("Bot: ", response, "\n")
+    }
+}
+
+
+# Model: llama3.2
+ollamar_chatbot("llama3.2")
+
+# Model: llama3.3
+ollamar_chatbot("llama3.3") # takes really long to run
