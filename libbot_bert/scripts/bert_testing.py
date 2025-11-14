c01a16e7110dd650248adea80bfdf5d204b1c276
Federico Aprile <fgaprile@ucdavis.edu>
2025-11-14 14:00:44 -0800

    embedding testing

--- libbot_bert/scripts/bert_testing.py
+++ libbot_bert/scripts/bert_testing.py
@@ -2,14 +2,41 @@ import torch
 from transformers import AutoTokenizer, AutoModel
 import pandas as pd
 
+# This directs where hugging face downloads the model
+# export HF_HOME=/dsl/libbot/data/huggingface_cache/ 
+
+
+
+
 df = pd.read_csv("/dsl/libbot/data/text_full_libguide.csv")
+num_rows, num_cols = df.shape
 
-checkpoint = "google-bert/bert-base-uncased"
+checkpoint = "bert-base-uncased"
 
+# loading tokenizer, model, and moving model to device (on server)
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-bert = AutoModel.from_pretrained(checkpoint)
+device = 0 if torch.cuda.is_available() else "cpu"
+print("Moved model to", device)
+bert_model = AutoModel.from_pretrained(checkpoint, device = device)
 
-sentence = "Then I tried to find some way of embracing my mother's ghost."
+bert_model.eval() #model in evaluation mode
 
-inputs = tokenizer(inputs, return_tensors = "pt", return_attention_mask = True, padding = "longest"
+
+
+# INPUT
+inputs = tokenizer(
+    sentence, return_tensors = "pt", return_attention_mask = True
 )
+
+
+"""
+Context manager => will keep the model from collecting gradients when it processes.
+Unless you are training a model or trying understand model internals, there’s no need for gradients.
+With the context manager built, send the inputs to the model.
+"""
+with torch.no_grad():
+    outputs = bert_model(**inputs, output_hidden_states = True)
+
+
+
+
