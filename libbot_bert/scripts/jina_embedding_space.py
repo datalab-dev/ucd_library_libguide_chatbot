75dd80e9a22612612409f29881d860e8cdb17bbc
Federico Aprile <fgaprile@ucdavis.edu>
2026-01-11 00:27:28 -0800

    updated jina embeddings script to also reflect the new changes (parquet usage, combined text column, etc.)

--- libbot_bert/scripts/jina_embedding_space.py
+++ libbot_bert/scripts/jina_embedding_space.py
@@ -7,46 +7,29 @@ import os
 os.environ["HF_HOME"] = "/dsl/libbot/data/huggingface_cache"
 os.environ["TRANSFORMERS_CACHE"] = "/dsl/libbot/data/huggingface_cache"
 
-
 # ---------- CONFIG ----------
-CSV_PATH = "/dsl/libbot/data/text_full_libguide.csv"
-TEXT_COL = "text"
-LIB_TITLE_COL = "libguide_title"
+# CSV_PATH = "/dsl/libbot/data/text_full_libguide.csv"
+P_PATH = "/dsl/libbot/data/combined_text_full_libguide.parquet"
+COMB_TEXT_COL = "combined_text"
 MODEL_NAME = "jinaai/jina-embeddings-v3"
 OUT_NPY = "/dsl/libbot/data/embeddings_jina_code.npy"
 BATCH_SIZE = 16   # adjust if needed
 # ----------------------------
+
+# Load text data
+df = pd.read_parquet(P_PATH)
+
+# prepare texts to embed
+texts = df[COMB_TEXT_COL].astype(str).tolist()
+
+print("Rows to embed:", len(texts))
+
 
 # Load model
 model = SentenceTransformer(
     MODEL_NAME,
     trust_remote_code=True,
     )
-
-# Load text data
-df = pd.read_csv(CSV_PATH, encoding='utf-8')
-
-# prepare texts + titles to embed
-lib_titles = df[LIB_TITLE_COL].fillna("").astype(str).tolist()
-text_chunks = df[TEXT_COL].fillna("").astype(str).tolist()
-
-texts = []   # combined strings
-
-for i in range(len(df)):
-    title = lib_titles[i]
-    text_chunk = text_chunks[i]
-
-    if title.strip() == "":
-        combined = text_chunk
-    else:
-        combined = title + ": " + text_chunk
-
-    texts.append(combined)
-
-
-print("Rows to embed:", len(texts))
-
-
 print("Model loaded:", MODEL_NAME)
 
 # Encode in batches
